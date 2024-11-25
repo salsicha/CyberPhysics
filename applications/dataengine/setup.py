ce4c3f95739ba83c0b65e34440ff851e419cde65
Alex Moran <salsicha@gmail.com>
2024-11-25 11:36:54 -0800

    Remove dataengine python package to separate repo

--- applications/dataengine/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="dataengine",
-    version="1.0",
-    description="Python package",
-    author="cyberphysics",
-    packages=find_packages(),
-)
