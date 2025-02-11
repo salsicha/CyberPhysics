9b4835663d895bf2d73927616248cc8a4145dedd
Alex Moran <salsicha@gmail.com>
2025-02-11 12:27:46 -0800

    Cythonize flight controller, cleanup

--- applications/pyflightcontroller/flightcontroller/cython/main.py
+++ applications/pyflightcontroller/flightcontroller/cython/main.py
@@ -333,36 +333,6 @@ def run() -> None:
 
 # UTILITY FUNCTIONS BELOW (Anything that is used by the flight controller loop should go here, not in a separate module or class (to save on processing time)
 
-def calculate_duty_cycle(throttle:float, dead_zone:float = 0.03) -> int:
-    """Determines the appropriate PWM duty cycle, in nanoseconds, to use for an ESC controlling a BLDC motor"""
-
-    ### SETTINGS (that aren't parameters) ###
-    duty_ceiling:int = 2000000 # the maximum duty cycle (max throttle, 100%) is 2 ms, or 10% duty (0.10)
-    duty_floor:int = 1000000 # the minimum duty cycle (min throttle, 0%) is 1 ms, or 5% duty (0.05). HOWEVER, I've observed some "twitching" at exactly 5% duty cycle. It is off, but occasionally clips above, triggering the motor temporarily. To prevent this, i'm bringing the minimum down to slightly below 5%
-    ################
-
-    # calcualte the filtered percentage (consider dead zone)
-    range:float = 1.0 - dead_zone - dead_zone
-    percentage:float = min(max((throttle - dead_zone) / range, 0.0), 1.0)
-    
-    dutyns:int = duty_floor + ((duty_ceiling - duty_floor) * percentage)
-
-    # clamp within the range
-    dutyns = max(duty_floor, min(dutyns, duty_ceiling))
-
-    return int(dutyns)
-
-def normalize(value:float, original_min:float, original_max:float, new_min:float, new_max:float) -> float:
-    """Normalizes (scales) a value to within a specific range."""
-    return new_min + ((new_max - new_min) * ((value - original_min) / (original_max - original_min)))
-
-def translate_pair(high:int, low:int) -> int:
-        """Converts a byte pair to a usable value. Borrowed from https://github.com/m-rtijn/mpu6050/blob/0626053a5e1182f4951b78b8326691a9223a5f7d/mpu6050/mpu6050.py#L76C39-L76C39."""
-        value = (high << 8) + low
-        if value >= 0x8000:
-            value = -((65535 - value) + 1)
-        return value   
-
 def FATAL_ERROR(msg:str) -> None:
     em:str = "Fatal error @ " + str(time.monotonic() * 1000) + " ms: " + msg
     print(em)
