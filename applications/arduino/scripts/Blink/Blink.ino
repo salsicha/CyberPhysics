d4f9e1b5f070c5d8f1e2b17feedc5983ed719eb9
Alex Moran <alex.moran@supernal.aero>
2025-04-09 00:38:29 -0700

    Fix blink.ino file

--- applications/arduino/scripts/Blink/Blink.ino
+++ applications/arduino/scripts/Blink/Blink.ino
@@ -1,28 +1,33 @@
-*
-   2  * rosserial Subscriber Example
-   3  * Blinks an LED on callback
-   4  */
-   5 
-   6 #include <ros.h>
-   7 #include <std_msgs/Empty.h>
-   8 
-   9 ros::NodeHandle nh;
-  10 
-  11 void messageCb( const std_msgs::Empty& toggle_msg){
-  12   digitalWrite(13, HIGH-digitalRead(13));   // blink the led
-  13 }
-  14 
-  15 ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCb );
-  16 
-  17 void setup()
-  18 {
-  19   pinMode(13, OUTPUT);
-  20   nh.initNode();
-  21   nh.subscribe(sub);
-  22 }
-  23 
-  24 void loop()
-  25 {
-  26   nh.spinOnce();
-  27   delay(1);
-  28 }
+ /*
+ * rosserial Subscriber Example
+ * Blinks an LED on callback
+ */
+
+#include <ros.h>
+#include <std_msgs/Empty.h>
+
+ros::NodeHandle nh;
+
+ /*
+ Note:
+ There is a built-in LED on pin 13
+ */
+
+void messageCb( const std_msgs::Empty& toggle_msg){
+  digitalWrite(13, HIGH-digitalRead(13));   // blink the led
+}
+
+ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCb );
+
+void setup()
+{
+  pinMode(13, OUTPUT);
+  nh.initNode();
+  nh.subscribe(sub);
+}
+
+void loop()
+{
+  nh.spinOnce();
+  delay(1);
+}
