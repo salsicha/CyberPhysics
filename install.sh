0c6fcbf963cd74586fad9cbe724f890f2fb6de09
Alex Moran <salsicha@gmail.com>
2025-05-05 14:24:10 -0700

    update container toolkit install method

--- install.sh
+++ install.sh
@@ -46,10 +46,18 @@ sudo mkdir /data
 # Enable Nvidia GPU passthrough
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L -k https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-curl -s -L -k https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+# curl -s -L -k https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
 sudo apt-get update && sudo apt-get install -y make nvidia-container-toolkit
 sudo nvidia-ctk runtime configure --runtime=docker
 sudo systemctl restart docker
+sudo systemctl daemon-reload
 
 # For Jetson emulation
 echo "Installing qemu (for Jetson platform target)"
