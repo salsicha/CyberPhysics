7e44c5b958a5de96c0c353faaef412bb4a4290c9
Alex Moran <salsicha@gmail.com>
2025-05-07 17:57:28 -0700

    comfyui mostly working

--- applications/comfyui/README.md
+++ applications/comfyui/README.md
@@ -1,23 +1,17 @@
 
+# ComfyUI and LTX Video
+
 https://github.com/comfyanonymous/ComfyUI?tab=readme-ov-file#manual-install-windows-linux
 
 https://github.com/Lightricks/LTX-Video?tab=readme-ov-file#inference
 
 
 
-TODO: figure out how to install these in the dockerfile:
-
-
-DOWNLOAD MODELS AND NODES:
-  
-
-Post install: Use ComfyUI-Manager. Simply search for ComfyUI-LTXVideo in list of nodes  
-
-Post install: Install one of the t5 text encoders, for example google_t5-v1_1-xxl_encoderonly. You can install it using ComfyUI Model Manager.  
-https://huggingface.co/mcmonkey/google_t5-v1_1-xxl_encoderonly/tree/main  
-
+## DOWNLOAD MODELS AND NODES:
 Post install: Need some additional custom nodes, like ComfyUI-VideoHelperSuite and others, installed, press "Install Missing Custom Nodes" button in ComfyUI Manager  
 
-Example workflow:  
+
+
+## Example workflow:  
 https://github.com/Lightricks/ComfyUI-LTXVideo/blob/master/example_workflows/ltxv-13b-i2v-base.json  
 
---
+++ applications/comfyui/prestart.py
@@ -0,0 +1,99 @@
+import comfy.options
+comfy.options.enable_args_parsing()
+
+import os
+import importlib.util
+import folder_paths
+import time
+from comfy.cli_args import args
+from app.logger import setup_logger
+import itertools
+import utils.extra_config
+import logging
+import sys
+
+if __name__ == "__main__":
+    #NOTE: These do not do anything on core ComfyUI, they are for custom nodes.
+    os.environ['HF_HUB_DISABLE_TELEMETRY'] = '1'
+    os.environ['DO_NOT_TRACK'] = '1'
+
+
+setup_logger(log_level=args.verbose, use_stdout=args.log_stdout)
+
+def apply_custom_paths():
+    # extra model paths
+    extra_model_paths_config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "extra_model_paths.yaml")
+    if os.path.isfile(extra_model_paths_config_path):
+        utils.extra_config.load_extra_path_config(extra_model_paths_config_path)
+
+    if args.extra_model_paths_config:
+        for config_path in itertools.chain(*args.extra_model_paths_config):
+            utils.extra_config.load_extra_path_config(config_path)
+
+    # --output-directory, --input-directory, --user-directory
+    if args.output_directory:
+        output_dir = os.path.abspath(args.output_directory)
+        logging.info(f"Setting output directory to: {output_dir}")
+        folder_paths.set_output_directory(output_dir)
+
+    # These are the default folders that checkpoints, clip and vae models will be saved to when using CheckpointSave, etc.. nodes
+    folder_paths.add_model_folder_path("checkpoints", os.path.join(folder_paths.get_output_directory(), "checkpoints"))
+    folder_paths.add_model_folder_path("clip", os.path.join(folder_paths.get_output_directory(), "clip"))
+    folder_paths.add_model_folder_path("vae", os.path.join(folder_paths.get_output_directory(), "vae"))
+    folder_paths.add_model_folder_path("diffusion_models",
+                                       os.path.join(folder_paths.get_output_directory(), "diffusion_models"))
+    folder_paths.add_model_folder_path("loras", os.path.join(folder_paths.get_output_directory(), "loras"))
+
+    if args.input_directory:
+        input_dir = os.path.abspath(args.input_directory)
+        logging.info(f"Setting input directory to: {input_dir}")
+        folder_paths.set_input_directory(input_dir)
+
+    if args.user_directory:
+        user_dir = os.path.abspath(args.user_directory)
+        logging.info(f"Setting user directory to: {user_dir}")
+        folder_paths.set_user_directory(user_dir)
+
+
+def execute_prestartup_script():
+    def execute_script(script_path):
+        module_name = os.path.splitext(script_path)[0]
+        try:
+            spec = importlib.util.spec_from_file_location(module_name, script_path)
+            module = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(module)
+            return True
+        except Exception as e:
+            logging.error(f"Failed to execute startup-script: {script_path} / {e}")
+        return False
+
+    if args.disable_all_custom_nodes:
+        return
+
+    node_paths = folder_paths.get_folder_paths("custom_nodes")
+    for custom_node_path in node_paths:
+        possible_modules = os.listdir(custom_node_path)
+        node_prestartup_times = []
+
+        for possible_module in possible_modules:
+            module_path = os.path.join(custom_node_path, possible_module)
+            if os.path.isfile(module_path) or module_path.endswith(".disabled") or module_path == "__pycache__":
+                continue
+
+            script_path = os.path.join(module_path, "prestartup_script.py")
+            if os.path.exists(script_path):
+                time_before = time.perf_counter()
+                success = execute_script(script_path)
+                node_prestartup_times.append((time.perf_counter() - time_before, module_path, success))
+    if len(node_prestartup_times) > 0:
+        logging.info("\nPrestartup times for custom nodes:")
+        for n in sorted(node_prestartup_times):
+            if n[2]:
+                import_message = ""
+            else:
+                import_message = " (PRESTARTUP FAILED)"
+            logging.info("{:6.1f} seconds{}: {}".format(n[0], import_message, n[1]))
+        logging.info("")
+
+apply_custom_paths()
+execute_prestartup_script()
