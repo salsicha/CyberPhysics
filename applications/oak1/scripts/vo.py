54a322b9c27bedfd71bef10d067952622f1528fd
Alex Moran <salsicha@gmail.com>
2026-02-09 08:40:41 -0800

    add comment

--- applications/oak1/scripts/vo.py
+++ applications/oak1/scripts/vo.py
@@ -1,5 +1,5 @@
 
-# TODO: implement visual odometry for the Oak-1 camera.
+# TODO: implement visual odometry for the Oak-1 camera using the depthai SDK.
 # The output will be unscaled/non-metric depth.
 # Then publish the depth map as a ROS topic.
 # Another ros node will fuse IMU and depth map to produce a scaled/metric depth map.
