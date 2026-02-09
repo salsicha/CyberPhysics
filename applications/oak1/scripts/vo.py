17a734c0a4ab02bbf1921595006b2e96c4fa09b2
Alex Moran <salsicha@gmail.com>
2026-02-09 08:40:28 -0800

    add comment

---
+++ applications/oak1/scripts/vo.py
@@ -0,0 +1,8 @@
+
+# TODO: implement visual odometry for the Oak-1 camera.
+# The output will be unscaled/non-metric depth.
+# Then publish the depth map as a ROS topic.
+# Another ros node will fuse IMU and depth map to produce a scaled/metric depth map.
+# The navigation stack will use the metric depth map for obstacle avoidance and path planning.
+ 
+
